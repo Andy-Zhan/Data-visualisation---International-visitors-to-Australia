4f3d17eefdf5c65a8f38f08e01e9a1e797a9a422
azha0018 <azha0018@student.monash.edu>
2021-10-07 21:24:47 +1100

    bonanza

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 $(function () {
-  vegaEmbed("#symbol_map", "../charts/map3.vg.json")
+  vegaEmbed("#symbol_map", "../charts/map4.vg.json")
     .then(function (result) {
       // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     })
     .catch(console.error);
 });
 
-$(function () {
-  vegaEmbed(
-    "#total_visitors_by_year",
-    "../charts/total_visitors_by_year.vg.json"
-  )
-    .then(function (result) {
-      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-    })
-    .catch(console.error);
-});
+// $(function () {
+//   vegaEmbed(
+//     "#total_visitors_by_year",
+//     "../charts/total_visitors_by_year.vg.json"
+//   )
+//     .then(function (result) {
+//       // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+//     })
+//     .catch(console.error);
+// });
 
 $(function () {
   vegaEmbed("#reasons_for_visiting", "../charts/reasons_for_visiting.vg.json")
