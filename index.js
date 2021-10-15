c2fc3c50ab284ab6926a9c0882895352846af326
azha0018 <azha0018@student.monash.edu>
2021-10-15 20:35:02 +1100

    chart 4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function () {
-  vegaEmbed("#symbol_map", "./charts/map4.vg.json")
+  vegaEmbed("#symbol_map", "./charts/map4.vg.json", { actions: false })
     .then(function (result) {
       // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     })
@@ -17,7 +17,9 @@ $(function () {
 // });
 
 $(function () {
-  vegaEmbed("#reasons_for_visiting", "./charts/reasons_for_visiting.vg.json")
+  vegaEmbed("#reasons_for_visiting", "./charts/reasons_for_visiting.vg.json", {
+    actions: false,
+  })
     .then(function (result) {
       // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     })
@@ -25,9 +27,29 @@ $(function () {
 });
 
 $(function () {
-  vegaEmbed("#monthly_visits", "./charts/monthly_visits.vg.json")
+  vegaEmbed("#monthly_visits", "./charts/monthly_visits.vg.json", {
+    actions: false,
+  })
     .then(function (result) {
       // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     })
     .catch(console.error);
+});
+
+$(function () {
+  vegaEmbed("#spend", "./charts/spend.vg.json", { actions: false })
+    .then(function (result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    })
+    .catch(console.error);
+
+  vega.expressionFunction("milFormat", function (datum, params) {
+    let n = datum / 1000000;
+    return n.toString() + "M";
+  });
+
+  vega.expressionFunction("moneyFormat", function (datum, params) {
+    let n = datum / 1000000000;
+    return "$" + n.toString() + "B";
+  });
 });
